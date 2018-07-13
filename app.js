5370e9a5cb27849e6c59c1672f6c00b210c20741
spotlight <31270657+thienanh96@users.noreply.github.com>
2018-07-13 17:27:31 +0700

    second commit
    second commit

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 const api = require('./router');
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const port = 3000;
 const cors = require('cors');
 //......
 
 var app = express();
 app.use(cors());
-app.use(bodyParser.urlencoded({
-    limit: '50mb'
-}));
-app.use(bodyParser.json({
-    limit: '50mb'
-}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', api);
 app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname + '/tool new.html'));
 })
 var server = require('http').Server(app);
-server.listen(port, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on port ' + port);
 })
