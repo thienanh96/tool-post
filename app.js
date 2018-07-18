7d7aca43f10a0242d92cb4b2a15bc4432c16be5a
spotlight <31270657+thienanh96@users.noreply.github.com>
2018-07-18 12:24:01 +0700

    2
    2

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', api);
-// app.get('/*',function(req,res){
-//     res.sendFile(path.join(__dirname + '/tool new.html'));
-// })
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname + '/tool new.html'));
+})
 var server = require('http').Server(app);
 server.listen(process.env.PORT || 3000, () => {
     let p  = server.address().port;
